c7620bf7537dcb9237e8d6dfaeabc80d482fb701
thanh5320 <thanh20183632@gmail.com>
2022-03-13 18:52:21 +0700

    demo

--- src/App.js
+++ src/App.js
@@ -100,20 +100,7 @@ function App(props) {
 
   return (
     <div className="todoapp stack-large">
-      <Form addTask={addTask} />
-      <div className="filters btn-group stack-exception">
-        {filterList}
-      </div>
-      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
-        {headingText}
-      </h2>
-      <ul
-        role="list"
-        className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading"
-      >
-        {taskList}
-      </ul>
+        Chào bạn
     </div>
   );
 }
